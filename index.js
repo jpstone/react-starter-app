e9134584a0d64aa6cf18ce37bc2e33596cfd14d0
Justin Stone <JustinStone@socadmins-MacBook-Pro.local>
2016-07-12 13:48:51 -0400

    first try

--- index.js
+++ index.js
@@ -1,69 +1,19 @@
-var Promise = require('bluebird');
-var fs = Promise.promisifyAll(require('fs'));
-var npm = Promise.promisifyAll(require('npm'));
 var cliColor = require('cli-color');
-var exec = require('child_process').exec;
+var gulp = require('gulp');
+var install = require('gulp-install');
 
 module.exports = scaffold;
 
 function scaffold() {
-  console.log(cliColor.blue('Scaffolding app...'));
-  readdirRecursive(__dirname + '/lib')
-  .map(function (item) {
-    var itemPath = item.path.split('react-starter-app/lib')[1];
-    if (item.isDirectory) {
-      return fs.mkdirAsync(process.cwd() + itemPath);
-    } else {
-      return fs.readFileAsync(item.path, 'utf8')
-        .then(function (contents) {
-          return fs.writeFileAsync(
-            process.cwd() + itemPath,
-            contents,
-            'utf8'
-          );
-        });
-    }
-  })
-  .all()
-  .then(function () {
-    console.log(cliColor.blue('Finished scaffolding app'));
-    console.log(cliColor.green('Installing npm modules...'));
-    exec('npm install ' + process.cwd(), function (err, stdout, stderr) {
-      if (err) console.log(cliColor.red(err));
-      if (stdout) console.log(stdout);
-      if (stderr) console.log(cliColor.red(stderr));
-    });
-  })
-  .catch(function (err) {
-    console.error(cliColor.red(err.stack));
-  });
-}
-
-function readdirRecursive(dir) {
-  return fs.readdirAsync(dir)
-  .reduce(function (paths, item) {
-    return fs.lstatAsync(dir + '/' + item)
-    .then(function (stats) {
-      if (stats.isFile()) {
-        paths.push({
-          path: dir + '/' + item,
-          stats: stats,
-          isFile: true
-        });
-      } else if (stats.isDirectory()) {
-        paths.push({
-          path: dir + '/' + item,
-          stats: stats,
-          isDirectory: true
-        });
-        return readdirRecursive(dir + '/' + item)
-        .each(function (results) {
-          paths.push(results);
-        });
-      }
-    })
-    .then(function () {
-      return paths;
-    })
-  }, []);
+  return gulp.src(__dirname + '/lib')
+    .pipe(gulp.dest(process.cwd()))
+    .pipe(console.log(cliColor.blue(
+      'Finished scaffolding app',
+      'Installing npm packages...'
+    )))
+    .pipe(install());
+    .pipe(console.log(cliColor.green(
+      'Finished installing npm packages',
+      'All done! Run "gulp serve" to start your app'
+    )));
 }
